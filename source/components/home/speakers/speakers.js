5f44cfb46c3b97e91de85ed7dd5517275421a19e
Ammar Ahmed <ahmedammar841@gmail.com>
2018-04-01 15:36:08 -0500

    added info button and removed extra play buttons

--- source/components/home/speakers/speakers.js
+++ source/components/home/speakers/speakers.js
@@ -12,83 +12,98 @@ class Speakers extends Component {
     render() {
 
         const speakerList = [
-            // Keynotes
+             // Keynotes
             {
                 'name': 'Steve Wozniak',
                 'img': '../assets/img/keynotes/steve.jpg',
-                'year': 1995
+                'year': 1995,
+                'wikipedia': 'https://en.wikipedia.org/wiki/Steve_Wozniak'
             },
             {
                 'name': 'Alexis Ohanian',
                 'img': '../assets/img/keynotes/alex.jpg',
-                'year': 2009
+                'year': 2009,
+                'wikipedia': 'https://en.wikipedia.org/wiki/Alexis_Ohanian'
             },
             {
                 'name': 'Brian J. Fox',
                 'img': '../assets/img/keynotes/brian.jpg',
                 'year': 2017,
                 'youtube': 'https://www.youtube.com/watch?v=itBU4F50N8Q',
+                'wikipedia': 'https://en.wikipedia.org/wiki/Brian_Fox_(computer_programmer)'
             },
             {
                 'name': 'Jay “Saurik” Freeman',
                 'img': '../assets/img/keynotes/jay.jpg',
-                'year': 2014
+                'year': 2014,
+                'wikipedia': 'https://en.wikipedia.org/wiki/Jay_Freeman'
             },
             {
                 'name': 'Peter Norvig',
                 'img': '../assets/img/keynotes/peter.jpg',
-                'year': 2013
+                'year': 2013,
+                'wikipedia': 'https://en.wikipedia.org/wiki/Peter_Norvig'
             },
             {
                 'name': 'Danielle Feinberg',
                 'img': '../assets/img/keynotes/danielle.jpg',
-                'year': 2012
+                'year': 2012,
+                'wikipedia': 'https://en.wikipedia.org/wiki/Danielle_Feinberg'
             },
             {
                 'name': 'Travis Oliphant',
                 'img': '../assets/img/keynotes/travis.jpg',
                 'year': 2017,
-                'youtube': 'https://www.youtube.com/watch?v=_PDvlqbgxCU'
+                'youtube': 'https://www.youtube.com/watch?v=_PDvlqbgxCU',
+                'wikipedia': 'https://en.wikipedia.org/wiki/Travis_Oliphant'
             },
             {
                 'name': 'Anna Patterson',
                 'img': '../assets/img/keynotes/anna.jpg',
-                'year': 2015
+                'year': 2015,
+                'wikipedia': 'https://en.wikipedia.org/wiki/Anna_Patterson'
             },
             {
                 'name': 'Robin Walker',
                 'img': '../assets/img/keynotes/robin.jpg',
-                'year': 2013
+                'year': 2013,
+                'wikipedia': 'https://en.wikipedia.org/wiki/Robin_Walker_(game_designer)'
             },
             {
                 'name': 'Laura I. Gómez',
                 'img': '../assets/img/keynotes/laura.jpg',
-                'year': 2016
+                'year': 2016,
+                'wikipedia': ''
             },
             {
                 'name': 'Basil Alwan',
                 'img': '../assets/img/keynotes/basil.jpg',
-                'year': 2016
+                'year': 2016,
+                'wikipedia': ''
             },
             {
                 'name': 'Cassidy Williams',
                 'img': '../assets/img/keynotes/cassidy.jpg',
-                'year': 2015
+                'year': 2015,
+                'wikipedia': ''
             },
             {
                 'name': 'Chris Gladwin',
                 'img': '../assets/img/keynotes/chris.jpg',
-                'year': 2017
+                'year': 2017,
+                'wikipedia': ''
             },
             {
                 'name': 'Stefano Zacchiroli',
                 'img': '../assets/img/keynotes/stefano.jpg',
-                'year': 2012
+                'year': 2012,
+                'wikipedia': ''
             },
             {
                 'name': 'Roy Bhat',
                 'img': '../assets/img/keynotes/roy.jpg',
-                'year': 2016
+                'year': 2016,
+                'wikipedia': ''
             }
         ];
 
@@ -117,26 +132,45 @@ class Speakers extends Component {
                 case 'keynote':
                     hex = "#9a6fb0"
                     break;
-
             }
             var hexStr = "6px solid " + hex;
             var link = speaker.youtube;
             var reveal = (link != undefined && link != null) ? "visible" : "hidden";
-            var cl = (link != undefined && link != null) ? "#c9e88f" : "#a4b0bc";
-            return ( 
-                <Col key = { idx } xs = { 12 } lg = { 4 } sm = { 9 } md = { 9 } >
-                    <Thumbnail onClick = {(e) => this.handleClick(e, link, idx) } className = "Speakers__card js-speakercard" src = { speaker.img } >
-                    <h3 style = {{ color: "white" } } > { speaker.name } </h3> 
-                    <h3 style = {{ color: "white" } } > { speaker.year } </h3> 
-                    <span className = "glyphicon glyphicon-play-circle" id = "youtube-icon" style = {{ color: cl } }/>  
-                    </Thumbnail> 
-                </Col>
-            )
+            //hasLinkColor - Color of glyphicon link if the speaker's card DOES LINK to a YouTube video
+            const hasLinkColor = "#c9e88f";
+            //hasNoLinkColor - Color of glyphicon link if the speaker's card DOES NOT LINK to a YouTube video
+            const hasNoLinkColor = "#a4b0bc";
+            var cl = (link != undefined && link != null) ? hasLinkColor : hasNoLinkColor;
+            var wikicolor = "#c9e88f"
+            var cName = (cl == hasLinkColor)? "Speakers__card js-speakercard Speakers__youtube": "Speakers__card js-speakercard";
+            if (link) {
+                return ( 
+                    <Col key = { idx } xs = { 12 } lg = { 4 } sm = { 9 } md = { 9 } >
+                        <Thumbnail onClick = {(e) => this.handleClick(e, link, idx) } className = {cName} src = { speaker.img } >
+                        <h3 style = {{ color: "white" } } > { speaker.name } </h3> 
+                        <h3 style = {{ color: "white" } } > { speaker.year } </h3> 
+                        <span className = "glyphicon glyphicon-play-circle" id = "youtube-icon" style = {{ color: cl } }/>
+                        <span className = "glyphicon glyphicon-info-sign" id = "wiki-icon" style = {{ color: wikicolor} }/>  
+                        </Thumbnail> 
+                    </Col>
+                )
+            } else {
+                return(
+                    <Col key = { idx } xs = { 12 } lg = { 4 } sm = { 9 } md = { 9 } >
+                        <Thumbnail onClick = {(e) => this.handleClick(e, link, idx) } className = {cName} src = { speaker.img } >
+                        <h3 style = {{ color: "white" } } > { speaker.name } </h3> 
+                        <h3 style = {{ color: "white" } } > { speaker.year } </h3> 
+                        <span className = "glyphicon glyphicon-play-circle" id = "youtube-icon" style = {{ color: cl, opacity: 0 } }/>  
+                        <span className = "glyphicon glyphicon-info-sign" id = "wiki-icon" style = {{ color: wikicolor} }/>
+                        </Thumbnail> 
+                    </Col>
+                )
+            }
         });
 
         return ( 
             <div className = "Speakers" id = "Speakers" >
-                <PageHeader className = "Speakers__header" > Past Keynote Speakers </PageHeader> 
+                <PageHeader className = "Speakers__header" > <h2>Past Speakers </h2></PageHeader> 
                 <Grid ><Row > { speakerView } </Row> </Grid> 
             </div>
         )
