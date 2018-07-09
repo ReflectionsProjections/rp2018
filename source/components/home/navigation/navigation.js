95b5f222d6ebaed0f0d559000c07175b2bb540c2
Pradyumna Shome <pradyumna.shome@gmail.com>
2018-07-08 20:33:59 -0400

    Moved "Contact Us" to the end of navbar

--- source/components/home/navigation/navigation.js
+++ source/components/home/navigation/navigation.js
@@ -36,9 +36,9 @@ class Navigation extends Component {
                         <NavItem onClick={() => this.scrollTo('About')} eventKey={1} className="Navigation__item--active">About</NavItem>
                         <NavItem onClick={() => this.scrollTo('Speakers')} eventKey={2} >Past Speakers</NavItem>
                         <NavItem onClick={() => this.scrollTo('Sponsors')} eventKey={3} >Past Sponsors</NavItem>
-                        <NavItem onClick={() => this.scrollTo('Footer')} eventKey={4} >Contact Us</NavItem>
-                        <NavItem onClick={() => location.href="https://blog.reflectionsprojections.org"} eventKey={5}>Blog</NavItem>
-                        <NavItem onClick={() => location.href="/#/faq"} eventKey={6}>FAQ</NavItem>
+                        <NavItem onClick={() => location.href="https://blog.reflectionsprojections.org"} eventKey={4}>Blog</NavItem>
+                        <NavItem onClick={() => location.href="/#/faq"} eventKey={5}>FAQ</NavItem>
+                        <NavItem onClick={() => this.scrollTo('Footer')} eventKey={6} >Contact Us</NavItem>
                     </Nav>
                     </Navbar.Collapse>
                 </Navbar>
