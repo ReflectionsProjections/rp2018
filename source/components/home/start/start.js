f1355bf96166fa74d7679b404536a91298e0093a
Pradyumna Shome <pradyumna.shome@gmail.com>
2018-07-17 19:57:57 -0400

    Fixed landing page carousel

--- source/components/home/start/start.js
+++ source/components/home/start/start.js
@@ -9,14 +9,14 @@ import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
 const PHOTO_SET = [
-  { src: '../assets/img/history/career0.jpg', alt: 'Slide 1' },
-  { src: '../assets/img/history/career2.jpg', alt: 'Slide 2' },
-  { src: '../assets/img/history/speaker0.jpg', alt: 'Slide 3' },
-  { src: '../assets/img/history/mechmania.jpg', alt: 'Slide 4' },
-  { src: '../assets/img/history/speaker1.jpg', alt: 'Slide 5' },
-  { src: '../assets/img/history/career3.jpg', alt: 'Slide 6' },
-  { src: '../assets/img/history/career4.jpg', alt: 'Slide 7' },
-  { src: '../assets/img/history/career5.jpg', alt: 'Slide 8' }
+  { src: '../../../assets/img/history/career0.jpg', alt: 'Slide 1' },
+  { src: '../../../assets/img/history/career2.jpg', alt: 'Slide 2' },
+  { src: '../../../assets/img/history/speaker0.jpg', alt: 'Slide 3' },
+  { src: '../../../assets/img/history/mechmania.jpg', alt: 'Slide 4' },
+  { src: '../../../assets/img/history/speaker1.jpg', alt: 'Slide 5' },
+  { src: '../../../assets/img/history/career3.jpg', alt: 'Slide 6' },
+  { src: '../../../assets/img/history/career4.jpg', alt: 'Slide 7' },
+  { src: '../../../assets/img/history/career5.jpg', alt: 'Slide 8' }
 ];
 
 const popoverBottom = (
@@ -46,23 +46,24 @@ class Start extends Component {
                 <Jumbotron>
                     <div id="carousel_container">
                         <Slider  {...settings}>
-                             {PHOTO_SET.map((item, index) => (
-                                 <div id="images_container">
-                                   <img id="carousel_image" src={item.src} alt={item.alt}/>
-                                 </div>
-                             ))}
+                                {PHOTO_SET.map((item, index) => (
+                                    <div id="images_container">
+                                        <img id="carousel_image" src={item.src} alt={item.alt}/>
+                                    </div>
+                                ))}
                         </Slider>
                     </div>
-
-                    <img className="header" src="../assets/svg/home_header.svg" />
-                    <div className="caps">September 17th - 22nd, 2018</div>
-                    <Countdown className="counter"/>
-                    {/*<div style={{margin: "15px", paddingBottom: "10px", fontSize: "18px"}}>DAYS&nbsp; LEFT</div>*/}
-                    <div className="intro"> Interested in sponsoring or attending? </div>
-                    <a className="btn" href="mailto:conference-chair@acm.illinois.edu">
-                        <button className="contact">CONTACT US
-                        </button>
-                    </a>
+                    <div id="carousel-overlay">
+                        <img className="header" src="../assets/svg/home_header.svg" />
+                        <div className="caps">September 17th - 22nd, 2018</div>
+                        <Countdown className="counter"/>
+                        {/*<div style={{margin: "15px", paddingBottom: "10px", fontSize: "18px"}}>DAYS&nbsp; LEFT</div>*/}
+                        <div className="intro"> Interested in sponsoring or attending? </div>
+                        <a className="btn" href="mailto:conference-chair@acm.illinois.edu">
+                            <button className="contact">CONTACT US
+                            </button>
+                        </a>
+                    </div>
                     <Scrolldown/>
                 </Jumbotron>
             </div>
