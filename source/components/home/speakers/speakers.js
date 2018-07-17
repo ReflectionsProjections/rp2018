1bde040dd2d58d1ae4bc6d5b6a67a3f40df4db1c
Ammar Ahmed <ahmedammar841@gmail.com>
2018-07-17 13:24:21 -0500

    Removed past speakers and switched to round portraits

--- source/components/home/speakers/speakers.js
+++ source/components/home/speakers/speakers.js
@@ -60,97 +60,6 @@ class Speakers extends Component {
                 'img': '../assets/img/keynotes/zac.jpg',
                 'year': 'ML Scientist at Amazon AI Labs, Professor CMU',
                 'wikipedia': 'http://zacklipton.com'
-            },
-            {
-                'name': ' ',
-                'img': '../assets/img/keynotes/past.png',
-                'year': ' ',
-            },
-            {
-                'name': 'Alexis Ohanian',
-                'img': '../assets/img/keynotes/alex.jpg',
-                'year': 'Co-founder of Reddit',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Alexis_Ohanian'
-            },
-            {
-                'name': 'Brian J. Fox',
-                'img': '../assets/img/keynotes/brian.jpg',
-                'year': 'Author of GNU Bash Shell',
-                'youtube': 'https://www.youtube.com/watch?v=itBU4F50N8Q',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Brian_Fox_(computer_programmer)'
-            },
-            {
-                'name': 'Jay “Saurik” Freeman',
-                'img': '../assets/img/keynotes/jay.jpg',
-                'year': 'Creator of Cydia',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Jay_Freeman'
-            },
-            {
-                'name': 'Peter Norvig',
-                'img': '../assets/img/keynotes/peter.jpg',
-                'year': 'Director of Research at Google Inc.',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Peter_Norvig'
-            },
-            {
-                'name': 'Danielle Feinberg',
-                'img': '../assets/img/keynotes/danielle.jpg',
-                'year': 'Director of Photography at Pixar',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Danielle_Feinberg'
-            },
-            {
-                'name': 'Travis Oliphant',
-                'img': '../assets/img/keynotes/travis.jpg',
-                'year': 'Founder of Anaconda & Creator of NumPy',
-                'youtube': 'https://www.youtube.com/watch?v=_PDvlqbgxCU',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Travis_Oliphant'
-            },
-            {
-                'name': 'Anna Patterson',
-                'img': '../assets/img/keynotes/anna.jpg',
-                'year': 'Co-founder of Search Engine Cuil',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Anna_Patterson'
-            },
-            {
-                'name': 'Robin Walker',
-                'img': '../assets/img/keynotes/robin.jpg',
-                'year': 'Co-developer of Team Fortress',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Robin_Walker_(game_designer)'
-            },
-            {
-                'name': 'Laura I. Gómez',
-                'img': '../assets/img/keynotes/laura.jpg',
-                'year': 'Founder and CEO of Atipica Inc.',
-                'wikipedia': 'https://www.linkedin.com/in/lauraigomez/'
-            },
-            {
-                'name': 'Basil Alwan',
-                'img': '../assets/img/keynotes/basil.jpg',
-                'year': 'President of IP/Optical Networks at Nokia',
-                'wikipedia': 'https://www.nokia.com/en_int/investors/corporate-governance/group-leadership-team/basil-alwan-president-of-ipoptical-networks'
-            },
-            {
-                'name': 'Cassidy Williams',
-                'img': '../assets/img/keynotes/cassidy.jpg',
-                'year': 'Head of Amazon Developer Voice Programs',
-                'wikipedia': 'https://cassidoo.co/'
-            },
-            {
-                'name': 'Chris Gladwin',
-                'img': '../assets/img/keynotes/chris.jpg',
-                'year': 'Founder of Cleversafe',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Chris_Gladwin_(engineer)'
-            },
-            {
-                'name': 'Stefano Zacchiroli',
-                'img': '../assets/img/keynotes/stefano.jpg',
-                'year': 'Debian Project Leader',
-                'wikipedia': 'https://upsilon.cc/~zack/'
-            },
-            {
-                'name': 'Roy Bahat',
-                'img': '../assets/img/keynotes/roy.jpg',
-                'year': 'Head of Bloomberg Beta',
-                'wikipedia': 'https://www.linkedin.com/in/roybahat/'
             }
         ];
 
@@ -192,7 +101,7 @@ class Speakers extends Component {
             if (wikilink) {
                 return ( 
                     <Col key = { idx } xs = { 12 } lg = { 4 } sm = { 6 } md = { 6 }>
-                        <Thumbnail className = {cName} src = { speaker.img } >
+                        <Thumbnail className = {cName} src = { speaker.img } class="img-circle" >
                         <h3 style = {{ color: "white" } } > { speaker.name } </h3> 
                         <h4 style = {{ color: "white" } } > { speaker.year } </h4> 
                         <div className="icon-group">
