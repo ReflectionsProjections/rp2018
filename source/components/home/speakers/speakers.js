8041d9254a408252a6683f96f370e93a5f9e0f58
modnikhil <mod.nikhil@gmail.com>
2018-07-24 18:58:17 -0500

    added keynote, marianna tessel, reordered speakers a little

--- source/components/home/speakers/speakers.js
+++ source/components/home/speakers/speakers.js
@@ -14,51 +14,58 @@ class Speakers extends Component {
         const speakerList = [
              // Keynotes
             {
-                'name': 'Siraj Raval',
-                'img': '../assets/img/keynotes/siraj.jpg',
-                'year': 'YouTuber - Accessible CS education',
-                'wikipedia': 'https://twitter.com/sirajraval'
+                'name': 'Marianna Tessel',
+                'img': '../assets/img/keynotes/marianna4.jpeg',
+                'title': 'Chief Product Development Officer at Intuit',
+                'wikipedia': 'https://twitter.com/mtessel12',
+                'track': 'keynote'
             },
             {
                 'name': 'Saad Ali',
                 'img': '../assets/img/keynotes/saad.jpg',
-                'year': 'Senior Software Engineer at Google',
+                'title': 'Senior Software Engineer at Google',
                 'wikipedia': 'https://twitter.com/the_saad_ali'
-            },
-            {
-                'name': 'Jay Gambetta',
-                'img': '../assets/img/keynotes/jay_g.jpg',
-                'year': 'Quantum Computing Researcher at IBM',
-                'wikipedia': 'https://en.wikipedia.org/wiki/Jay_Gambetta'
-            },
-            {
-                'name': 'Simon Adler',
-                'img': '../assets/img/keynotes/simon.jpg',
-                'year': 'Producer at Radiolab',
-                'wikipedia': 'https://www.wnycstudios.org/people/simon-adler/'
             },
             {
                 'name': 'Brian Hoffman',
                 'img': '../assets/img/keynotes/brain_h.jpg',
-                'year': 'CEO/Founder OpenBazaar',
+                'title': 'CEO/Founder OpenBazaar',
                 'wikipedia': 'https://twitter.com/brianchoffman'
             },
             {
                 'name': 'Forrest Iandola',
                 'img': '../assets/img/keynotes/forrest.jpg',
-                'year': 'CEO Deepscale.ai',
+                'title': 'CEO Deepscale.ai',
                 'wikipedia': 'https://en.wikipedia.org/wiki/Forrest_Iandola'
+            },
+            {
+                'name': 'Siraj Raval',
+                'img': '../assets/img/keynotes/siraj.jpg',
+                'title': 'YouTuber - Accessible CS education',
+                'wikipedia': 'https://twitter.com/sirajraval'
+            },
+            {
+                'name': 'Jay Gambetta',
+                'img': '../assets/img/keynotes/jay_g.jpg',
+                'title': 'Quantum Computing Researcher at IBM',
+                'wikipedia': 'https://en.wikipedia.org/wiki/Jay_Gambetta'
+            },
+            {
+                'name': 'Simon Adler',
+                'img': '../assets/img/keynotes/simon.jpg',
+                'title': 'Producer at Radiolab',
+                'wikipedia': 'https://www.wnycstudios.org/people/simon-adler/'
             },
             {
                 'name': 'Romain Paulus',
                 'img': '../assets/img/keynotes/romain.jpg',
-                'year': 'Chief ML Scientist at Salesforce',
+                'title': 'Chief ML Scientist at Salesforce',
                 'wikipedia': 'https://www.linkedin.com/in/romainpaulus/?locale=en_US'
             },
             {
                 'name': 'Zachary Lipton',
                 'img': '../assets/img/keynotes/zac.jpg',
-                'year': 'ML Scientist at Amazon AI Labs, Assistant Professor CMU',
+                'title': 'ML Scientist at Amazon AI Labs, Assistant Professor CMU',
                 'wikipedia': 'http://zacklipton.com'
             }
         ];
@@ -103,7 +110,7 @@ class Speakers extends Component {
                     <Col key = { idx } xs = { 12 } lg = { 3 } sm = { 6 } md = { 6 }>
                         <Thumbnail className = {cName} src = { speaker.img } >
                         <h3 style = {{ color: "white" } } > { speaker.name } </h3>
-                        <h4 style = {{ color: "white" } } > { speaker.year } </h4>
+                        <h4 style = {{ color: "white" } } > { speaker.title } </h4>
                         <div className="icon-group">
                             <a href= {wikilink} target="_blank" rel="noopener noreferrer">
                                 <span className = "glyphicon glyphicon-info-sign" id = "wiki-icon" style = {{ color: wikicolor} }/>
@@ -117,7 +124,7 @@ class Speakers extends Component {
                     <Col key = { idx } xs = { 12 } lg = { 3 } sm = { 6 } md = { 6 }>
                         <Thumbnail className = {cName} src = { speaker.img } >
                         <h3 style = {{ color: "white" } } > { speaker.name } </h3>
-                        <h4 style = {{ color: "white" } } > { speaker.year } </h4>
+                        <h4 style = {{ color: "white" } } > { speaker.title } </h4>
                         <div className="icon-group">
                         </div>
                         </Thumbnail>
