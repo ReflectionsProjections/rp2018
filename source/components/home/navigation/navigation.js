33abeb2913a00a95559bbadeeb43a001f5fac187
modnikhil <mod.nikhil@gmail.com>
2018-07-26 16:33:11 -0500

    reordered navbar

--- source/components/home/navigation/navigation.js
+++ source/components/home/navigation/navigation.js
@@ -37,16 +37,16 @@ class Navigation extends Component {
                     <Navbar.Collapse>
                     <Nav pullRight>
                         <NavItem onClick={() => this.scrollTo('About')} eventKey={1} className="Navigation__item--active">About</NavItem>
-                        <NavItem onClick={() => this.scrollTo('Speakers')} eventKey={2} >Speakers</NavItem>
-                        <NavItem onClick={() => this.scrollTo('Sponsors')} eventKey={3} >Sponsors</NavItem>
-                        <NavItem onClick={() => location.href="https://blog.reflectionsprojections.org"} eventKey={4}>Blog</NavItem>
-                        <NavDropdown eventKey={5} title="Events" id="basic-nav-dropdown"  onMouseEnter={(e) => this.setState({ show: true })}
+                        <NavDropdown eventKey={2} title="Events" id="basic-nav-dropdown"  onMouseEnter={(e) => this.setState({ show: true })}
                         onMouseLeave={(e) => this.setState({ show: false })} open={this.state.show}>
-                            <MenuItem onClick={() => location.href="/#/puzzlebang"} eventKey={5.1}>PuzzleBang</MenuItem>
-                            <MenuItem onClick={() => location.href="/#/mechmania"} eventKey={5.2}>MechMania</MenuItem>
-                            <MenuItem onClick={() => location.href="/#/symposium"} eventKey={5.3}>ACM Symposium</MenuItem>
+                            <MenuItem onClick={() => location.href="/#/puzzlebang"} eventKey={2.1}>PuzzleBang</MenuItem>
+                            <MenuItem onClick={() => location.href="/#/mechmania"} eventKey={2.2}>MechMania</MenuItem>
+                            <MenuItem onClick={() => location.href="/#/symposium"} eventKey={2.3}>ACM Symposium</MenuItem>
                         </NavDropdown>
-                        <NavItem onClick={() => location.href="/#/faq"} eventKey={6}>FAQ</NavItem>
+                        <NavItem onClick={() => this.scrollTo('Speakers')} eventKey={3} >Speakers</NavItem>
+                        <NavItem onClick={() => this.scrollTo('FAQ')} eventKey={4}>FAQ</NavItem>
+                        <NavItem onClick={() => this.scrollTo('Sponsors')} eventKey={5} >Sponsors</NavItem>
+                        <NavItem onClick={() => location.href="https://blog.reflectionsprojections.org"} eventKey={6}>Blog</NavItem>
                     </Nav>
                     </Navbar.Collapse>
                 </Navbar>
