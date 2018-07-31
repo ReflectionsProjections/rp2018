8941e631db16ebdfb9cb26f950447343c53604a1
Pradyumna Shome <pradyumna.shome@gmail.com>
2018-07-30 21:36:49 -0400

    Replaced slider with tabs, added 2018 schedule

--- source/components/home/schedule/schedule.js
+++ source/components/home/schedule/schedule.js
@@ -6,21 +6,25 @@ class Schedule extends Component {
     constructor() {
         super();
         this.state = {
-            "scheduleType": "Corporate Events"
+            "scheduleType": "Corporate Events",
         }
         this.toggleSchedule = this.toggleSchedule.bind(this)
     }
 
+    componentDidMount() {
+        this.activeTab = document.body.querySelector(".active")
+    }
+
     toggleSchedule (event) {
-        if (this.state.scheduleType === "Corporate Events") {
-            this.setState({
-                "scheduleType": "Conference Events"
-            })
-        } else {
-            this.setState({
-                "scheduleType": "Corporate Events"
-            })
-        }
+        // Remove the active class from the previous active tab
+        this.activeTab.classList.remove("active")
+        this.activeTab = event.currentTarget
+        
+        // Add the active class to the current active tab
+        this.activeTab.classList.add("active")
+        this.setState({
+            "scheduleType": this.activeTab.innerHTML
+        })
     }
  
     render() {
@@ -33,54 +37,33 @@ class Schedule extends Component {
                         </h1>
                     </div>
                     <div className="content-container">
-                        <div className="switch-container">
-                            <label className="switch">
-                                <input type="checkbox" onClick={this.toggleSchedule}></input>
-                                <span className="slider round"><div className="label">{this.state.scheduleType}</div></span>
-                            </label>
+                        <div className="tab-container">
+                            <div className="tab tab-1 active" onClick={this.toggleSchedule}>
+                                Corporate Events
+                            </div>
+                            <div className="tab tab-2" onClick={this.toggleSchedule}>
+                                Conference Events
+                            </div>
                         </div>
                         <div className="content">
                             <div className="col">
                                 <ul>
-                                    <h4>Thu, Oct 1</h4>
-                                    <li><small>4:00p</small>Startup Panel<span>2405</span></li>
-                                    <li><small>5:00p</small>Startup Fair<span>Atrium</span></li>
-                                    <li><small>8:00p</small>Speaker: Roy Bahat<span>1404</span></li>
+                                    <h4>Mon, Sep 17</h4>
+                                    <li><small>12:00p</small>Granular Tech Talk<span>2405</span></li>
+                                    <li><small>6:00p</small>Salesforce Tech Talk<span>2405</span></li>
+                                    <li><small>7:00p</small>Corporate Social<span>Other</span></li>
                                 </ul>
                                 <ul>
-                                    <h4>Fri, Oct 2</h4>
-                                    <li><small>9:00a</small>Breakfast<span>Atrium</span></li>
-                                    <li><small>10:00a</small>Job Fair<span>Atrium</span></li>
-                                    <li><small>5:00p</small>Speaker: Craig Reynolds<span>1404</span></li>
-                                    <li><small>6:00p</small>Dinner/GoDaddy Talk<span>Atrium</span></li>
-                                    <li><small>7:00p</small>Speaker: Katherine Scott<span>1404</span></li>
-                                    <li><small>8:00p</small>Speaker: Gopal Kakivaya<span>1404</span></li>
-                                    <li><small>9:00p</small>Alumni Event</li>
-                                    <li><small>9:00p</small>Mechmania Opening<span>1404</span></li>
-                                </ul>
-                            </div>
-                            <div className="col">
-                                <ul>
-                                    <h4>Sat, Oct 3</h4>
-                                    <li><small>9:00a</small>Breakfast<span>Atrium</span></li>
-                                    <li><small>10:00a</small>Speaker: Jason Tan<span>1404</span></li>
-                                    <li><small>11:00a</small>Speaker: Jerry Talton<span>1404</span></li>
-                                    <li><small>12:00p</small>Lunch/Microsoft Talk<span>Atrium</span></li>
-                                    <li><small>1:00p</small>Speaker: Cassidy Williams<span>1404</span></li>
-                                    <li><small>2:00p</small>Speaker: Michael Armbrust<span>1404</span></li>
-                                    <li><small>3:00p</small>Speaker: Yisong Yue<span>1404</span></li>
-                                    <li><small>4:00p</small>Speaker: Winnie Cheng<span>1404</span></li>
-                                    <li><small>5:00p</small>Speaker: Alan Braverman<span>1404</span></li>
-                                    <li><small>6:00p</small>Dinner/Palantir Talk<span>Atrium</span></li>
-                                    <li><small>7:00p</small>Speaker: Anna Patterson<span>1404</span></li>
-                                    <li><small>8:00p</small>Speaker: Greg Baugues<span>1404</span></li>
-                                    <li><small>9:00p</small>Puzzlebang! Closing<span>2405</span></li>
+                                    <h4>Tue, Sep 18</h4>
+                                    <li><small>10:00a</small>Startup Fair<span>Atrium</span></li>
+                                    <li><small>6:00p</small>Uber Tech Talk<span>2405</span></li>
+                                    <li><small>7:00p</small>Corporate Social<span>Other</span></li>
                                 </ul>
                                 <ul>
-                                    <h4>Sun, Oct 4</h4>
-                                    <li><small>9:00a</small>Breakfast<span>Atrium</span></li>
-                                    <li><small>11:30a</small>Lunch<span>Atrium</span></li>
-                                    <li><small>12:30p</small>Mechmania Closing<span>1404</span></li>
+                                    <h4>Wed, Sep 19</h4>
+                                    <li><small>10:00a</small>Career Fair<span>Atrium</span></li>
+                                    <li><small>6:00p</small>Microsoft Tech Talk<span>2405</span></li>
+                                    <li><small>7:00p</small>Corporate Social<span>Other</span></li>
                                 </ul>
                             </div>
                         </div>
@@ -92,58 +75,59 @@ class Schedule extends Component {
                 <div id="Schedule" className="Schedule">
                     <div className="header">
                         <h1>
-                            This is supposed to be a different schedule.
+                            Schedule
                         </h1>
                     </div>
                     <div className="content-container">
-                        <div className="switch-container">
-                            <label className="switch">
-                                <input type="checkbox" onClick={this.toggleSchedule}></input>
-                                <span className="slider round"><div className="label">{this.state.scheduleType}</div></span>
-                            </label>
+                        <div className="tab-container">
+                            <div className="tab tab-1 active" onClick={this.toggleSchedule}>
+                                Corporate Events
+                            </div>
+                            <div className="tab tab-2" onClick={this.toggleSchedule}>
+                                Conference Events
+                            </div>
                         </div>
                         <div className="content">
                             <div className="col">
                                 <ul>
-                                    <h4>Thu, Oct 1</h4>
-                                    <li><small>4:00p</small>Startup Panel<span>2405</span></li>
-                                    <li><small>5:00p</small>Startup Fair<span>Atrium</span></li>
-                                    <li><small>8:00p</small>Speaker: Roy Bahat<span>1404</span></li>
+                                    <h4>Thu, Sep 20</h4>
+                                    <li><small>4:00p</small>Speaker Talk<span>2405</span></li>
+                                    <li><small>5:00p</small>Speaker Talk<span>2405</span></li>
                                 </ul>
                                 <ul>
-                                    <h4>Fri, Oct 2</h4>
-                                    <li><small>9:00a</small>Breakfast<span>Atrium</span></li>
-                                    <li><small>10:00a</small>Job Fair<span>Atrium</span></li>
-                                    <li><small>5:00p</small>Speaker: Craig Reynolds<span>1404</span></li>
-                                    <li><small>6:00p</small>Dinner/GoDaddy Talk<span>Atrium</span></li>
-                                    <li><small>7:00p</small>Speaker: Katherine Scott<span>1404</span></li>
-                                    <li><small>8:00p</small>Speaker: Gopal Kakivaya<span>1404</span></li>
-                                    <li><small>9:00p</small>Alumni Event</li>
-                                    <li><small>9:00p</small>Mechmania Opening<span>1404</span></li>
+                                    <h4>Fri, Sep 21</h4>
+                                    <li><small>11:00a</small>PuzzleBang<span>2nd Floor Atrium</span></li>
+                                    <li><small>1:00p</small>Speaker Talk<span>2405</span></li>
+                                    <li><small>1:00p</small>OSS<span>Other</span></li>
+                                    <li><small>2:00p</small>Speaker Talk<span>2405</span></li>
+                                    <li><small>4:00p</small>Speaker Talk<span>2405</span></li>
+                                    <li><small>5:00p</small>Speaker Talk<span>2405</span></li>
+                                    <li><small>6:00p</small>Speaker Talk<span>2405</span></li>
+                                    <li><small>7:00p</small>Dinner<span>Atrium</span></li>
+                                    <li><small>9:00p</small>MechMania Opening<span>1404</span></li>
+                                    <li><small>10:00p</small>Start of MechMania<span>1404</span></li>
+                                    <li><small>10:00p</small>Speaker Socials<span>Other</span></li>
+                                    <li><small>11:00p</small>Late night snack<span>Atrium</span></li>
                                 </ul>
                             </div>
                             <div className="col">
                                 <ul>
-                                    <h4>Sat, Oct 3</h4>
-                                    <li><small>9:00a</small>Breakfast<span>Atrium</span></li>
-                                    <li><small>10:00a</small>Speaker: Jason Tan<span>1404</span></li>
-                                    <li><small>11:00a</small>Speaker: Jerry Talton<span>1404</span></li>
-                                    <li><small>12:00p</small>Lunch/Microsoft Talk<span>Atrium</span></li>
-                                    <li><small>1:00p</small>Speaker: Cassidy Williams<span>1404</span></li>
-                                    <li><small>2:00p</small>Speaker: Michael Armbrust<span>1404</span></li>
-                                    <li><small>3:00p</small>Speaker: Yisong Yue<span>1404</span></li>
-                                    <li><small>4:00p</small>Speaker: Winnie Cheng<span>1404</span></li>
-                                    <li><small>5:00p</small>Speaker: Alan Braverman<span>1404</span></li>
-                                    <li><small>6:00p</small>Dinner/Palantir Talk<span>Atrium</span></li>
-                                    <li><small>7:00p</small>Speaker: Anna Patterson<span>1404</span></li>
-                                    <li><small>8:00p</small>Speaker: Greg Baugues<span>1404</span></li>
-                                    <li><small>9:00p</small>Puzzlebang! Closing<span>2405</span></li>
-                                </ul>
-                                <ul>
-                                    <h4>Sun, Oct 4</h4>
-                                    <li><small>9:00a</small>Breakfast<span>Atrium</span></li>
-                                    <li><small>11:30a</small>Lunch<span>Atrium</span></li>
-                                    <li><small>12:30p</small>Mechmania Closing<span>1404</span></li>
+                                    <h4>Sat, Sep 22</h4>
+                                    <li><small>8:00a</small>Breakfast<span>Atrium</span></li>
+                                    <li><small>8:00a</small>PuzzleBang<span>2nd Floor Atrium</span></li>
+                                    <li><small>10:00a</small>Speaker Tech Talk<span>2405</span></li>
+                                    <li><small>11:00a</small>Speaker Tech Talk<span>2405</span></li>
+                                    <li><small>12:00p</small>Lunch<span>Atrium</span></li>
+                                    <li><small>1:00p</small>Speaker Tech Talk<span>2405</span></li>
+                                    <li><small>3:00p</small>Speaker Tech Talk<span>2405</span></li>
+                                    <li><small>3:00p</small>Speaker Tech Talk<span>Other</span></li>
+                                    <li><small>4:00p</small>Speaker Tech Talk<span>2405</span></li>
+                                    <li><small>5:00p</small>ACM Symposium<span>2405</span></li>
+                                    <li><small>7:00p</small>Dinner<span>Atrium</span></li>
+                                    <li><small>8:00p</small>Speaker Socials<span>Other</span></li>
+                                    <li><small>8:00p</small>PuzzleBang Ends<span>2405</span></li>
+                                    <li><small>9:00p</small>MechMania Ends<span>1404</span></li>
+                                    <li><small>10:00p</small>MechMania Closing Ceremony<span>1404</span></li>
                                 </ul>
                             </div>
                         </div>
