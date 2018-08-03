3427489078903ad1b35d9aac128c8aeee2d779e0
modnikhil <mod.nikhil@gmail.com>
2018-08-03 00:16:49 -0500

    minor style changes to schedule

--- source/components/home/schedule/schedule.js
+++ source/components/home/schedule/schedule.js
@@ -19,14 +19,14 @@ class Schedule extends Component {
         // Remove the active class from the previous active tab
         this.activeTab.classList.remove("active")
         this.activeTab = event.currentTarget
-        
+
         // Add the active class to the current active tab
         this.activeTab.classList.add("active")
         this.setState({
             "scheduleType": this.activeTab.innerHTML
         })
     }
- 
+
     render() {
         if(this.state.scheduleType === "Corporate Events") {
             return (
@@ -48,22 +48,22 @@ class Schedule extends Component {
                         <div className="content">
                             <div className="col">
                                 <ul>
-                                    <h4>Mon, Sep 17</h4>
-                                    <li><small>12:00p</small>Granular Tech Talk<span>2405</span></li>
-                                    <li><small>6:00p</small>Salesforce Tech Talk<span>2405</span></li>
-                                    <li><small>7:00p</small>Corporate Social<span>Other</span></li>
+                                    <h3>Mon, Sep 17</h3>
+                                    <li><small>12:00p - 1:00p</small>Tech Talk: Granular<span>2405</span></li>
+                                    <li><small>6:00p - 7:00p</small>Tech Talk: Salesforce<span>2405</span></li>
+                                    <li><small>7:00p - 8:00p</small>Corporate Social: TBD<span>2405</span></li>
                                 </ul>
                                 <ul>
-                                    <h4>Tue, Sep 18</h4>
-                                    <li><small>10:00a</small>Startup Fair<span>Atrium</span></li>
-                                    <li><small>6:00p</small>Uber Tech Talk<span>2405</span></li>
-                                    <li><small>7:00p</small>Corporate Social<span>Other</span></li>
+                                    <h3>Tue, Sep 18</h3>
+                                    <li><small>10:00a - 4:00p</small>Startup Fair<span>Atrium</span></li>
+                                    <li><small>6:00p - 7:00p</small>Tech Talk: Uber<span>2405</span></li>
+                                    <li><small>7:00p - 8:00p</small>Corporate Social: Flexport<span>2405</span></li>
                                 </ul>
                                 <ul>
-                                    <h4>Wed, Sep 19</h4>
-                                    <li><small>10:00a</small>Career Fair<span>Atrium</span></li>
-                                    <li><small>6:00p</small>Microsoft Tech Talk<span>2405</span></li>
-                                    <li><small>7:00p</small>Corporate Social<span>Other</span></li>
+                                    <h3>Wed, Sep 19</h3>
+                                    <li><small>10:00a - 4:00p</small>Career Fair<span>Atrium</span></li>
+                                    <li><small>6:00p - 7:00p</small>Tech Talk: Microsoft<span>2405</span></li>
+                                    <li><small>7:00p - 8:00p</small>Corporate Social: TBD<span>2405</span></li>
                                 </ul>
                             </div>
                         </div>
@@ -90,44 +90,39 @@ class Schedule extends Component {
                         <div className="content">
                             <div className="col">
                                 <ul>
-                                    <h4>Thu, Sep 20</h4>
-                                    <li><small>4:00p</small>Speaker Talk<span>2405</span></li>
-                                    <li><small>5:00p</small>Speaker Talk<span>2405</span></li>
+                                    <h3>Thu, Sep 20</h3>
+                                    <li><small>4:00p - 5:00p</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>5:00p - 7:00p</small>Experience: TBA<span>2405</span></li>
                                 </ul>
                                 <ul>
-                                    <h4>Fri, Sep 21</h4>
-                                    <li><small>11:00a</small>PuzzleBang<span>2nd Floor Atrium</span></li>
-                                    <li><small>1:00p</small>Speaker Talk<span>2405</span></li>
-                                    <li><small>1:00p</small>OSS<span>Other</span></li>
-                                    <li><small>2:00p</small>Speaker Talk<span>2405</span></li>
-                                    <li><small>4:00p</small>Speaker Talk<span>2405</span></li>
-                                    <li><small>5:00p</small>Speaker Talk<span>2405</span></li>
-                                    <li><small>6:00p</small>Speaker Talk<span>2405</span></li>
+                                    <h3>Fri, Sep 21</h3>
+                                    <li><small>11:00a</small>PuzzleBang Comes to UIUC!<span>TBA</span><br/><center>Sponsored by IMO</center></li>
+                                    <li><small>1:00p - 2:00p</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>2:00p - 4:00p</small>Experience: TBA<span>2405</span></li>
+                                    <li><small>4:00p - 5:00p</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>5:00p - 6:00p</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>6:00p - 7:00p</small>Speaker: TBA<span>2405</span></li>
                                     <li><small>7:00p</small>Dinner<span>Atrium</span></li>
-                                    <li><small>9:00p</small>MechMania Opening<span>1404</span></li>
-                                    <li><small>10:00p</small>Start of MechMania<span>1404</span></li>
-                                    <li><small>10:00p</small>Speaker Socials<span>Other</span></li>
-                                    <li><small>11:00p</small>Late night snack<span>Atrium</span></li>
+                                    <li><small>9:00p</small>MechMania Opening Ceremony<span>1404</span><br/><center>Sponsored by DeepScale</center></li>
+                                    <li><small>10:00p</small>Speaker Socials<span>TBD</span></li>
+                                    <li><small>11:00p</small>Late Night Snack<span>Atrium</span></li>
                                 </ul>
                             </div>
                             <div className="col">
                                 <ul>
-                                    <h4>Sat, Sep 22</h4>
+                                    <h3>Sat, Sep 22</h3>
                                     <li><small>8:00a</small>Breakfast<span>Atrium</span></li>
-                                    <li><small>8:00a</small>PuzzleBang<span>2nd Floor Atrium</span></li>
-                                    <li><small>10:00a</small>Speaker Tech Talk<span>2405</span></li>
-                                    <li><small>11:00a</small>Speaker Tech Talk<span>2405</span></li>
+                                    <li><small>10:00a - 2:00a</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>11:00a - 2:00p</small>Speaker: TBA<span>2405</span></li>
                                     <li><small>12:00p</small>Lunch<span>Atrium</span></li>
-                                    <li><small>1:00p</small>Speaker Tech Talk<span>2405</span></li>
-                                    <li><small>3:00p</small>Speaker Tech Talk<span>2405</span></li>
-                                    <li><small>3:00p</small>Speaker Tech Talk<span>Other</span></li>
-                                    <li><small>4:00p</small>Speaker Tech Talk<span>2405</span></li>
-                                    <li><small>5:00p</small>ACM Symposium<span>2405</span></li>
+                                    <li><small>1:00p - 3:00p</small>Experience: TBA<span>2405</span></li>
+                                    <li><small>3:00p - 4:00p</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>4:00p - 5:00p</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>5:00p - 7:00p</small>ACM Symposium<span>2405</span></li>
                                     <li><small>7:00p</small>Dinner<span>Atrium</span></li>
-                                    <li><small>8:00p</small>Speaker Socials<span>Other</span></li>
-                                    <li><small>8:00p</small>PuzzleBang Ends<span>2405</span></li>
-                                    <li><small>9:00p</small>MechMania Ends<span>1404</span></li>
-                                    <li><small>10:00p</small>MechMania Closing Ceremony<span>1404</span></li>
+                                    <li><small>8:00p</small>Speaker Socials<span>TBD</span></li>
+                                    <li><small>8:00p</small>PuzzleBang Closing Ceremony<span>2405</span></li>
+                                    <li><small>9:00p</small>MechMania Closing Ceremony<span>1404</span></li>
                                 </ul>
                             </div>
                         </div>
