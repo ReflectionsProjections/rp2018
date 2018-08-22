581a79c49d4f6b6230aa44de15b7ad69f99a8fc7
modnikhil <mod.nikhil@gmail.com>
2018-08-22 17:52:26 -0500

    reverted back to old footer

--- source/components/home/footer/footer.js
+++ source/components/home/footer/footer.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { PageHeader, Grid, Row, Media, Glyphicon } from 'react-bootstrap'
 import styles from './styles.scss'
-
 const FA = require('react-fontawesome')
 
 class Footer extends Component {
@@ -16,53 +15,22 @@ class Footer extends Component {
 			height: "120px",
 			margin: "-10px 0 0 0"
 		};
-
-		return (
+		return(
 			<div className="Footer container" id="Footer">
 				<Media>
 					<Media.Left>
-						<img style={logoStyle} src="../assets/svg/acm_logo_50.svg" alt="Image" />
+						<img style={logoStyle} src="../assets/svg/acm_logo_50.svg" alt="Image"/>
 					</Media.Left>
 					<Media.Body>
 						<p className="Footer__contact">
-							<p>
-								<a>
-									<FA name='phone' /> +1 (217) 333-5828
-								</a>
-							</p>
-							<p>
-								<a href="mailto:conference-chair@acm.illinois.edu">
-									<FA name='envelope' /> conference-chair@acm.illinois.edu
-							</a>
-							</p>
-							<p>
-								<a href="https://www.facebook.com/acmrp/" target="_blank" rel="noopener noreferrer">
-									<FA name='facebook' /> ACM Reflections|Projections Conference
-							</a>
-							</p>
-							<p>
-								<a href="https://medium.com/reflections-projections" target="_blank" rel="noopener noreferrer"><FA name='medium' /> Reflections|Projections
-							</a>
-							</p>
-							<p>
-								<a href="https://twitter.com/uiuc_rp" target="_blank" rel="noopener noreferrer">
-									<FA name='twitter' />uiuc_rp </a>
-							</p>
-							<p>
-								<a href="https://www.reddit.com/user/uiuc_rp" target="_blank" rel="noopener noreferrer">
-									<FA name='reddit' />uiuc_rp
-							</a>
-							</p>
-							<p>
-								<a href="https://www.instagram.com/uiuc_rp/" target="_blank" rel="noopener noreferrer">
-									<FA name='instagram' />uiuc_rp
-							</a>
-							</p>
-							<p>
-								<a id="snapshot"><FA name='snapchat-ghost' /> uiuc_rp
-									<img className="tooltiptext" src="../assets/img/snapcode.png" />
-								</a>
-							</p>
+							<a><FA name='phone'/> +1 (217) 333-5828 </a><br />
+							<a href="mailto:conference-chair@acm.illinois.edu"><FA name='envelope'/> conference-chair@acm.illinois.edu </a> <br/>
+							<a href="https://www.facebook.com/acmrp/" target="_blank" rel="noopener noreferrer"><FA name='facebook' /> ACM Reflections|Projections Conference </a> <br/>
+							<a href="https://medium.com/reflections-projections" target="_blank" rel="noopener noreferrer"><FA name='medium' /> Reflections|Projections </a> <br/>
+							<a href="https://twitter.com/uiuc_rp" target="_blank" rel="noopener noreferrer"><FA name='twitter'/> @uiuc_rp </a>
+							<a href="https://www.reddit.com/user/uiuc_rp" target="_blank" rel="noopener noreferrer"><FA name='reddit' /> uiuc_rp </a><br/>
+							<a href="https://www.instagram.com/uiuc_rp/" target="_blank" rel="noopener noreferrer"><FA name='instagram' /> @uiuc_rp </a>
+							<a id="snapshot"><FA name='snapchat-ghost' /> uiuc_rp <img className="tooltiptext" src="../assets/img/snapcode.png" /></a><br/>
 						</p>
 					</Media.Body>
 				</Media>
