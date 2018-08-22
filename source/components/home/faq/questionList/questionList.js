e2e806fe09bc86c833d1c3429561e29a0e69a3fb
ChristopherRSettles <ChristopherRSettles@gmail.com>
2018-08-22 12:00:16 -0500

    Added an FAQ section for gas reimbursements.

--- source/components/home/faq/questionList/questionList.js
+++ source/components/home/faq/questionList/questionList.js
@@ -136,14 +136,6 @@ class QuestionList extends Component {
                     <ul className="column">
                         <li>
                             <div className="question">
-                                Are there travel reimbursements?
-                            </div>
-                            <div className="answer">
-                                We are currently reaching out to universities and community colleges in the midwest for interest in travel. If enough interest is shown from a certain area, we will gladly provide free transportation to UIUC for R|P! Please reach out to <a href="mailto://conference-chair@acm.illinois.edu">conference-chair@acm.illinois.edu</a> if you or your members are interested in attending R|P 2018.
-                            </div>
-                        </li>
-                        <li>
-                            <div className="question">
                                 Coming from Willard Airport
                             </div>
                             <div className="answer">
@@ -164,6 +156,54 @@ class QuestionList extends Component {
                             </div>
                             <div className="answer">
                                 Most amenities are available within walking distance of the Siebel Center. However, if walking isn't for you then Champaign-Urbana's public transit system can take you where you need to go. Routes can be mapped using Google Maps or the CUMTD app and each ride will cost you $1. No change is given on the buses, so bring exact fare with you!
+                            </div>
+                        </li>
+                    </ul>
+                    <h2 className="faq-heading">Attendee Gas Reimbursements</h2>
+                    <ul className="column">
+                        <li>
+                            <div className="question">
+                                How do I ask for a reimbursement?
+                            </div>
+                            <div className="answer">
+                            <ul>
+                                <li>The registration form has a transportation question that asks the user how they intend on getting to campus, one of the options being (Driving and will need Gas Reimbursements) </li>
+                                <li>We are offering reimbursements to the first 200 attendees that sign up for one. Afterwards, reimbursements will be case by case, with Reflections|Projections staff having final say.</li>
+                            </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="question">
+                                How much money does an attendee get?
+                            </div>
+                            <div className="answer">
+                            <ul>
+                                <li><strong> $15 </strong> per attendee if location is beyond 160 miles </li> 
+                                <li><strong> $10 </strong> per attendee if location is between 60 and 160 miles </li> 
+                                <li><strong> $5  </strong> per attendee if location is within 60 miles of campus </li> 
+                                <li>All distances are as the crow flies and RP staff has final say on which radius level your departure location falls in </li> 
+                                <li>These numbers are per attendee so they incentivize attendees to carpool </li>
+                            </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="question">
+                                How will I know if I'm within the first 200 people to sign up for GR?
+                            </div>
+                            <div className="answer">
+                                Assuming you have registered, we will send you an email by September 17th notifying you of your status. If you sign up after that date, we will do our best to notify you as soon as possible but cannot guarantee immediate confirmation
+                            </div>
+                        </li>
+                        <li>
+                            <div className="question">
+                                How do I collect my reimbursement?
+                            </div>
+                            <div className="answer">
+                            <ul>
+                                <li>We will hold a gas reimbursements booth on both Friday and Saturday. You must have explicitly requested reimbursements on your registration to be given money </li> 
+                                <li>You MUST have your School ID as proof that you are from the school you claim to be from. </li>
+                                <li>We will be reimbursing via Venmo and cash </li>
+                            </ul>
                             </div>
                         </li>
                     </ul>
