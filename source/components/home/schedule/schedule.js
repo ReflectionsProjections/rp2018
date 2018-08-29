52422af0dd1448b8950f6ff93eb8f00953887bd8
modnikhil <mod.nikhil@gmail.com>
2018-08-29 11:29:41 -0500

    replaced Marianna pic, added event specifics to schedule

--- source/components/home/schedule/schedule.js
+++ source/components/home/schedule/schedule.js
@@ -51,7 +51,7 @@ class Schedule extends Component {
                                     <h3>Mon, Sep 17</h3>
                                     <li><small>12:00p - 1:00p</small>Tech Talk: Granular<span>2405</span></li>
                                     <li><small>6:00p - 7:00p</small>Tech Talk: Salesforce<span>2405</span></li>
-                                    <li><small>7:00p - 8:00p</small>Corporate Social: TBD<span>2405</span></li>
+                                    <li><small>7:00p - 8:00p</small>Corporate Social: Qualtrics<span>2405</span></li>
                                 </ul>
                                 <ul>
                                     <h3>Tue, Sep 18</h3>
@@ -63,7 +63,7 @@ class Schedule extends Component {
                                     <h3>Wed, Sep 19</h3>
                                     <li><small>10:00a - 4:00p</small>Career Fair<span>Atrium</span></li>
                                     <li><small>6:00p - 7:00p</small>Tech Talk: Microsoft<span>2405</span></li>
-                                    <li><small>7:00p - 8:00p</small>Corporate Social: TBD<span>2405</span></li>
+                                    <li><small>7:00p - 8:00p</small>Corporate Social: Pinterest<span>2405</span></li>
                                 </ul>
                             </div>
                         </div>
@@ -91,18 +91,18 @@ class Schedule extends Component {
                             <div className="col">
                                 <ul>
                                     <h3>Thu, Sep 20</h3>
-                                    <li><small>4:00p - 5:00p</small>Speaker: TBA<span>2405</span></li>
-                                    <li><small>5:00p - 7:00p</small>Experience: TBA<span>2405</span></li>
+                                    <li><small>4:00p - 5:00p</small>Speaker: Josh Benaloh<span>2405</span></li>
+                                    <li><small>5:00p - 7:00p</small>Experience: Saad Ali<span>2405</span></li>
                                     <li><small>7:00p</small>Dinner<span>Atrium</span></li>
                                 </ul>
                                 <ul>
                                     <h3>Fri, Sep 21</h3>
                                     <li><small>11:00a</small>PuzzleBang Comes to UIUC!<span>TBA</span><br/><center>Sponsored by IMO</center></li>
                                     <li><small>1:00p - 2:00p</small>Speaker: TBA<span>2405</span></li>
-                                    <li><small>2:00p - 4:00p</small>Experience: TBA<span>2405</span></li>
-                                    <li><small>4:00p - 5:00p</small>Speaker: TBA<span>2405</span></li>
-                                    <li><small>5:00p - 6:00p</small>Speaker: TBA<span>2405</span></li>
-                                    <li><small>6:00p - 7:00p</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>2:00p - 4:00p</small>Experience: Forrest Iandola<span>2405</span></li>
+                                    <li><small>4:00p - 5:00p</small>Speaker: Jay Gambetta<span>2405</span></li>
+                                    <li><small>5:00p - 6:00p</small>Speaker: Romain Paulus<span>2405</span></li>
+                                    <li><small>6:00p - 7:00p</small>Keynote Speaker: Marianna Tessel<span>2405</span></li>
                                     <li><small>7:00p</small>Dinner<span>Atrium</span></li>
                                     <li><small>9:00p</small>MechMania Opening Ceremony<span>1404</span><br/><center>Sponsored by DeepScale</center></li>
                                     <li><small>9:00p</small>Speaker Socials<span>TBD</span></li>
@@ -113,12 +113,12 @@ class Schedule extends Component {
                                 <ul>
                                     <h3>Sat, Sep 22</h3>
                                     <li><small>8:00a</small>Breakfast<span>Atrium</span></li>
-                                    <li><small>10:00a - 11:00a</small>Speaker: TBA<span>2405</span></li>
-                                    <li><small>11:00a - 12:00p</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>10:00a - 11:00a</small>Speaker: Siraj Raval<span>2405</span></li>
+                                    <li><small>11:00a - 12:00p</small>Speaker: Simon Adler<span>2405</span></li>
                                     <li><small>12:00p</small>Lunch<span>Atrium</span></li>
-                                    <li><small>1:00p - 3:00p</small>Experience: TBA<span>2405</span></li>
-                                    <li><small>3:00p - 4:00p</small>Speaker: TBA<span>2405</span></li>
-                                    <li><small>4:00p - 5:00p</small>Speaker: TBA<span>2405</span></li>
+                                    <li><small>1:00p - 3:00p</small>Experience: Brian Hoffman<span>2405</span></li>
+                                    <li><small>3:00p - 4:00p</small>Speaker: Jonah Horowitz<span>2405</span></li>
+                                    <li><small>4:00p - 5:00p</small>Speaker: Zack Lipton<span>2405</span></li>
                                     <li><small>5:00p - 7:00p</small>ACM Symposium<span>2405</span></li>
                                     <li><small>7:00p</small>Dinner<span>Atrium</span></li>
                                     <li><small>8:00p</small>PuzzleBang Closing Ceremony<span>2405</span></li>
