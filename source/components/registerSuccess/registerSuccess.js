ca55427227f147ad68c7ad7e912cc26c95a8a129
Vidushi Rai <vidushi97@gmail.com>
2018-09-04 00:30:18 -0500

    changed text on the successful register page

--- source/components/registerSuccess/registerSuccess.js
+++ source/components/registerSuccess/registerSuccess.js
@@ -12,10 +12,10 @@ export default class RegisterSuccess extends Component {
               <Image style={{margin: 'auto'}} src='../assets/2018logo.png' size='small' />
             </Grid.Row>
             <Grid.Row className='successPrompt verticalPadding'>
-              You have succesfully registered for Reflections | Projections! You should receive an email from us shortly with your <b>unique conference QR code</b>. Be sure to check your <b>spam folder</b>. Check our website for information on the schedule of events! If you have any questions, please email us at <a href='mailto:conference-chair@acm.illinois.edu'>conference-chair@acm.illinois.edu</a>
+              You have succesfully registered for Reflections | Projections! You should receive an email from us shortly with your <b>unique conference QR code</b>. Be sure to check your <b>spam folder</b>. Check our website for information on the schedule of events! If you have any questions, please email us at <a href='mailto:contact@reflectionsprojections.org'>contact@reflectionsprojections.org</a>
             </Grid.Row>
             <Grid.Row className='successPrompt verticalPadding'>
-               Don't forget to register for the <a href="https://acmrp.org/symposium/register" target="_blank">ACM Symposium</a>, <a href="https://acmrp.typeform.com/to/MR6XmN" target="_blank">MechMania</a> or <a href="https://puzzlebang.com" target="_blank">PuzzleBang</a>
+               Don't forget to register for the <a href="https://acmrp.org/symposium/register" target="_blank"><u>ACM Symposium</u></a>, <a href="https://acmrp.typeform.com/to/MR6XmN" target="_blank"><u>MechMania</u></a> or <a href="https://puzzlebang.com" target="_blank"><u>PuzzleBang</u></a>
             </Grid.Row>
             <Grid.Row className='verticalPadding'>
               <a href="/"><Button>Home</Button></a>
